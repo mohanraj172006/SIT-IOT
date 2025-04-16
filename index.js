3550f1d71532ca7624a9e4b5b4b783fe62f4d853
MOHAN RAJ G <mohanrajg172006@gmail.com>
2025-04-16 08:27:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// Example: Adding play/pause functionality to the video
+document.querySelectorAll('video').forEach(video => {
+    video.addEventListener('click', () => {
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
+  });
+  // Add smooth scrolling
+  document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
